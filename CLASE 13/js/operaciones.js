ac645f6b2b56da63fc1384a38599c0629ed3a320
elCapellan <lucasmedina@outlook.com>
2022-09-15 19:59:38 -0300

    clase 13

---
+++ CLASE 13/js/operaciones.js
@@ -0,0 +1,19 @@
+/*codigo js*/
+        //comentario en js
+        function saludar(){
+            alert('Hola a todo el mundo');
+        }
+
+
+/*OPERADORES*/
+//CUENTAS MATEMÁTICAS
+//variables
+let op1 = prompt('Ingrese el valor de Op1');//string o cadena
+let op2 = prompt('Ingrese el valor de Op2');
+let res = op1+op2
+res=(+op1) + parseInt(op2);
+//console.log(1+4);
+//muestro resultado por consola
+console.log(res);
+//muestro resultado por alerta
+//alert(res); //muestra cartel en pantalla
